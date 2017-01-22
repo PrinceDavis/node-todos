74377537de3d1a9a3c2d83b30172c8de6e52e2a5
Ossaija ThankGod <codebugsolved@gmail.com>
2017-01-22 13:24:36 +0100

    refactoring

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 /*jshint esversion: 6 */
 
 const mongoose = require('mongoose');
-const dbUri = process.env.MONGODB_URI | 'mongodb://localhost:27017/TodoApp';
+const dbUri = process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp';
 mongoose.Promise = global.Promise;
 mongoose.connect(dbUri);
 
