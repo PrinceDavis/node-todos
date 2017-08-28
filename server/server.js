66d21945ce7503d79441c9a1550b1e12bda76a94
Ossaija ThankGod <codebugsolved@gmail.com>
2017-08-28 16:31:36 +0100

    added default endpoint

--- server/server.js
+++ server/server.js
@@ -113,7 +113,7 @@ app.post('/users', (req, res) => {
 
 
 //get all users
-app.get('/users', (req, res) => {
+app.get('/users', authenticate, (req, res) => {
 	User.find().then((users) => {
 		res.send(users);
 	}).catch((err) => res.status(400).send(err));
